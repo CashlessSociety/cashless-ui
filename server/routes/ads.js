f0ba4b8ad12e8fbe91d325039f8dcbdb3691a828
Miles Thompson <utunga@gmail.com>
2021-01-16 22:11:48 +1300

    Mess around with headers add a minimal route for ads (which we probably dont need)

---
+++ server/routes/ads.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const ads = [
+  {
+    src: "assets/img/pp-1.jpg",
+    altText: "",
+    caption: "",
+  },
+  {
+    src: "assets/img/pp-2.jpg",
+    altText: "",
+    caption: "",
+  },
+  {
+    src: "assets/img/pp-3.jpg",
+    altText: "",
+    caption: "",
+  },
+  {
+    src: "assets/img/pp-4.jpg",
+    altText: "",
+    caption: "",
+  },
+];
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(ads);
+});
+
+module.exports = router;
