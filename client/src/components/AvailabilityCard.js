fc459fff8c97c59161cd61359243605c75286acd
Miles Thompson <utunga@gmail.com>
2021-01-21 16:18:18 +1300

    Add AvailabilityCard and make it a switch

---
+++ client/src/components/AvailabilityCard.js
@@ -0,0 +1,49 @@
+
+
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+// reactstrap components
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Button,
+} from "reactstrap";
+
+
+export const AVAILABILITY_CARD_DATA = gql`
+  fragment AvailabilityCardData on AvailabilityMessage {
+    skills {
+      name
+    }
+    author {
+      commonName {
+        name
+      }
+    }
+  }
+`;
+
+
+export const AvailabilityCard = ({ message })  => {
+    console.log(message)
+    return (
+    <>
+      <Card>
+         <CardBody>
+            <p>
+              {/* FIXME whats the best way to do a comma separated list ? */}
+            {message.author.commonName.name} is available for 
+                    {message.skills.map((skill) => 
+                        <>{skill.name}</>
+                    )}
+            </p>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+
+  
