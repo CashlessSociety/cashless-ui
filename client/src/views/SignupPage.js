7a6971fbcee1988e118db8f69a996e0d21232799
Sarah Rogers <srogersnzdev@gmail.com>
2021-03-30 10:15:26 +1300

    replace footer, remove comments

--- client/src/views/SignupPage.js
+++ client/src/views/SignupPage.js
@@ -14,21 +14,11 @@ function SignupPage({ isAuthenticated, storageKeys }) {
   return (
     <>
       <TopNavbar isAuthenticated={isAuthenticated} />
-
-      {/* 
-        this places content below the navbar and centers it. 
-        it changes the font colour from black to white, which seems to be a css styling thing when .page-header and .container are combined... 
-        not sure what else it's doing... 
-        pulled it from creativetim example signup page component styling to fix problem of content disappearing under navbar 
-        .page-header:before - this seems to be resonsible for making the background grey */}
       <div className="page-header">
-      {/* <div className='wrapper'> */}
         <div className="content">
-
           <SignupForm storageKeys={storageKeys} />
-          {/* removing this for now as it's so huge and silly! and the content spills over the top of it confusingly */}
-          {/* <FooterSocial /> */}
         </div>
+        <FooterSocial />
       </div>
     </>
   );
